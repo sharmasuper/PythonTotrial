6a22776ca176eb2f7bdb52942c7fb0949f9c87dc
sharmasuper <mohit6375sharma@gmail.com>
2024-05-31 09:17:27 +0530

    touple method

--- text.py
+++ text.py
@@ -1,33 +1,41 @@
-# list method
-L = [11,45,1,2,4,6]
-# print(L)
-# L.append(3)
-# print(L)
-# print(L.index(3))
-# print(L.count(2))
-# difference between copy method
+# # tuple is immutable 
+# # tup = (1,2,4,3,4,5)
+# # print(tup)
 
-# m = L
-# m[0] = 0
-# print(m)
-# print(L) you looked, frist element of both element are changed
-# but if we will use copy mehod, frist list will not change
-# m = L.copy()
-# m[0] = 0
-# print(L)
-# print(m)
+# # tup = (1)
+# # print(type(tup)) //output - class 'int'
+# # so we write comma
+# tup = (1,)
+# print(type(tup))
+# tup = (1,30,33,40)
+# # tup[1] = 20
+# # print(tup[1])   tuples are imutable we only can get tuple value
+# # print(tup[-1]) 
+# if 30 in tup :
+#     # print("hello yes ")
 
-# L.insert(index,element)
-# L.insert(1,99)
-# print(L)
+# countries = ("Spain","Italy","India","EngLand")
+# countries2 = ("1","hello","sharma")
+# print(countries+countries2)
 
-# extend
-m = [900,1000,1100]
-# L.extend(m)
-# print(L)
+touple = (0,11,2,3,3,4,3)
+tup = touple.count(3)
+print(tup)
+e = touple.index(3,1,4)
+print(e)
 
-K = L+m
-print(K)
+print("len of tuple ",len(touple))
+
+# change touple to list 
+countries = ("hello","hy","name","MOHIT")
+a= list(countries)
+print(a) 
+# now we can be use it as list mehod
+a = set(list)
+print(a)
+
+
+
 
 
 
