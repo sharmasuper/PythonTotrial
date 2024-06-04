2395973ba0a96993f3d1eaf5ccd821703a1408d2
sharmasuper <mohit6375sharma@gmail.com>
2024-06-04 19:41:35 +0530

    enumerate() method

--- text.py
+++ text.py
@@ -1,3 +1,7 @@
-a = 300
-b = 300
-print("A") if a>b else print("=") if a == b else print("B") 
+
+marks = ["hllo","hy","Manii",'Ram']
+#enumerate method index detaha h value ki
+for index,mark in enumerate(marks) :
+    print(mark)
+    if(index == 3) :
+        print("Harry , awesome !")
