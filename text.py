624c6a866685a11ff561f33a1d45dcf160865d5c
sharmasuper <mohit6375sharma@gmail.com>
2024-06-04 20:13:03 +0530

    virtual environment

--- text.py
+++ text.py
@@ -1,7 +1,19 @@
 
-marks = ["hllo","hy","Manii",'Ram']
-#enumerate method index detaha h value ki
-for index,mark in enumerate(marks) :
-    print(mark)
-    if(index == 3) :
-        print("Harry , awesome !")
+virtual Environment
+
+frist step = frist,we will build own python folder
+second step = second , we create own pthon-enev folder dynmicly in python folder 
+             so we will write command - [python -m venv myenev]
+third step = we will activate this folder and we can search in chatgpt, how activate - command - [.\Scripts\Activate] 
+       for chaching purpuse - pip3 install pandas                                                                                                     
+
+ four step = [deactivate]    for deactivate purpace
+check install package - [pip freeze]
+ agar hami kiskai folder mai install package chahiy to hum simply likh sktai h - [pip freeze > requirements.txt]      
+ isssai ek python file banai gi or srai  install packages aa jaygai
+
+   ageer yai folder second dost kai pass jata h to vo sirf yai ho likaiga - [pip install -r requirements.txt]   
+   uskai fiolder mai yai package aa jaygai  
+
+
+
