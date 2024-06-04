7e3e28d3dabd4401f79beb358c20d06eabc53bd7
sharmasuper <mohit6375sharma@gmail.com>
2024-06-04 14:00:03 +0530

    raise method

--- text.py
+++ text.py
@@ -1,18 +1,3 @@
-def funck1():
- try :
-    l = [1,2,3,4]
-    i = int(input("Enter the Index "))
-    print(l[i])
-    return "true"
- except : 
-      print('Some error find') 
-      return 0
-# except Exception as e :
-#      print("some error find",e)
- finally : 
-      print("i am always excecuted")      #it return karai jab bhi excecute hoga jabki easi print mai likhnai per exceute nahi hoga   
-      return "hel"
- print("i am always executed")
-
-val = funck1()
-print(val)
+a = int(input("Enter any value between 9 "))
+if(a<5 or a>9) :
+    raise "value error h"
