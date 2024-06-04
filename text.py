8038a36c872328e8cd3dae51b6ee9fbfa7a60e0c
sharmasuper <mohit6375sharma@gmail.com>
2024-06-04 12:06:49 +0530

    dictionary

--- text.py
+++ text.py
@@ -1,54 +1,17 @@
-# # # set method
-# # s1 = {1,2,5,6}
-# # s2 = {3,6,7}
-# # # union method
-# # print(s1.union(s2))  
-# # s1.update(s2)
-# # print(s1)
-# # # difference between upadte method and union method is - 
-# # s = s1.union(s2)
-# # print(s)
-
-# # s1.update(s2)
-# # print(s1)
-
-# # # intersection method
-# # cities = {"Tokyo","Madrid","Berlin","Delhi"}
-# # cities2 = {"Tokyo","seoul","kabul","Madhrid"}
-# # cities3 = cities.intersection(cities2)
-# # print(cities3)
-# # # intersection, this method gives which value are both common in cities and cities2 set
-# # semitric difference
-# cities = {"Tokyo","Madrid","Berlin","Delhi"}
-# cities2 = {"Tokyo","Seoul","Kabul","Madrid"}
-# # cities3 = cities.symmetric_difference(cities2)
-# # print(cities3) 
-
-# # output - {'Delhi', 'Berlin', 'Seoul', 'Kabul'}
-# # cities3 = cities.difference(cities2)
-# # print(cities3) 
-# # print(cities.isdisjoint(cities2))  if everyone element does not match both of them it return true
-# cities3 = {"Tokyo","Madrid","Delhi"}
-# print(cities.issuperset(cities3))
-# # it means cities3 elements are in cities if it have it give true else false
-
-# print(cities3.issubset(cities))  
-
-
-# cities = {"Tokyo","Madrid","Berlin","Delhi"}
-# cities.remove('Tokyo')  it give error when element does not match
-# print(cities)
-
-cities = {"Tokyo","Madrid","Berlin","Delhi"}
-# cities.discard('Tokyo2')  it not give error when element does not match
-# print(cities)
-a = cities.pop()
-print(a)
-print(cities)
-del cities
-print(cities)
-
-
+info = {'name' : "Karna","age":"19","eligible":True}
+print("dic pri",info)
+print(info['name'])  #yai error show kartha h element nahi honai per
+print(info.get('name2')) #yai error show nahi kartha 
+print(info.keys()) # these method we get all keys
+print(info.values())
+# for key in info.values() :# info.keys()
+#     # print(info[key])
+#     # print(value)
+#     print(key)
+   
+for key,value in info.items() :
+    print(key)
+    print(value)
 
 
 
