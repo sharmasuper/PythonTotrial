315ca34692b632ad8cde9db30bc2c22b4d97d8e9
sharmasuper <mohit6375sharma@gmail.com>
2024-06-04 13:13:41 +0530

    finally method for error handlinng

--- text.py
+++ text.py
@@ -1,8 +1,18 @@
-tablevalue = input("enter value you want to convert in table  ")
+def funck1():
+ try :
+    l = [1,2,3,4]
+    i = int(input("Enter the Index "))
+    print(l[i])
+    return "true"
+ except : 
+      print('Some error find') 
+      return 0
+# except Exception as e :
+#      print("some error find",e)
+ finally : 
+      print("i am always excecuted")      #it return karai jab bhi excecute hoga jabki easi print mai likhnai per exceute nahi hoga   
+      return "hel"
+ print("i am always executed")
 
-try :
-     for i in range(1,11):
-          print(f"{i} * {tablevalue} = > {int(i)*int(tablevalue)}")
-except Exception as e :
-     print("give error ",e)          
-       
+val = funck1()
+print(val)
