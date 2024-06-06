b9e92f04029003b8c72c84d8b9dce01f274adca5
sharmasuper <mohit6375sharma@gmail.com>
2024-06-07 00:30:46 +0530

    os module in python

--- text.py
+++ text.py
@@ -1,9 +1,13 @@
-import Hello
-Hello.welcome() 
+import  os
 
+# if(not os.path.exists('data')):    
+# os.mkdir('data')
+try :
+   for i in range(0, 100):
+      os.rename(f"data/day{i+1}.txt",f"data/mohit{i+1}.txt") 
 
-
-
+except Exception as e :
+     print("add",e)
 
 
 
