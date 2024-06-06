012b3c4c0ece09e5e5d97757951cffcccf25ceb0
sharmasuper <mohit6375sharma@gmail.com>
2024-06-07 03:54:30 +0530

    map filter and reduce method in python

--- text.py
+++ text.py
@@ -1,12 +1,41 @@
+try :
+    def cube(x) :
+        return x**3
+      
+    
+    newl = [1,2,3,4,5,6]
+    # nell = []
+    # for i in newl :
+    #     nell.append(cube(i))
+    # print(nell)  
+    # Map(function,item) method //item - one by one take item
+    newl2 = list(map(cube,newl))
+    print(newl2)
+
+    #filter
+    def filter_function (a) :
+        return a>4
+    newnewl = list(filter(filter_function,newl))
+    print(newnewl)
+
+
+# except Exception as e :
+#     print("add Error",e)
+
+#reduce funciton 
+
 try:
-    double = lambda arguments: arguments*2
-    avg  = lambda x,y : (x+y)/2
-    cube = lambda three : three**3
-    print(double(5)) 
-    print(cube(5))
-    print(avg(3,5))
-    # what is Annoymous - 
-    # those function called Annoymous ,which does not know about name 
+    from functools  import reduce
+    numbers = [1,2,3,4,5]
+    def mysum(x,y) :
+      return x + y
+      #reduce(function,element,starting value)
+    sum = reduce(mysum,numbers,5)  
+    print('sum',sum) 
 
 except Exception as e :
     print("add Error",e)    
+
+
+
+
