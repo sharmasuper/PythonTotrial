1af2b9812a2d8354a4020975904f27a9b13c843e
sharmasuper <mohit6375sharma@gmail.com>
2024-06-07 02:15:38 +0530

    read() readlines() and other file methods

--- text.py
+++ text.py
@@ -1,16 +1,41 @@
-try:
+try :
+    f = open('myfile.txt','r')
+    i =0
+    while True:
+        i=i+1
+        line = f.readline()
+        m1 =  line.split(",")[0]
+        m2 =  line.split(",")[1]
+        m3 =  line.split(",")[2]
+        print(f"Marks of student {i} in maths {m1}")
+        print(f"Marks of student {i} in Englisj {m2}")
+        print(f"Marks of student {i} in sst {m3}")
+        if not line :
+           break
+        print(line)        
+except Exception as  e:
+    print("show  error",e) 
 
-#  f = open('data.txt','r') reading file
-#  text = f.read()
-#  print(text)
 
-#  f = open('myfile.txt','a')
-#  f.write('hello,world  add!')  # new file have create and when we run agin compiler this text agin add myfile.txt
- 
-#  f.close()
- # new method
- with open('mynewfile.txt','a') as f :
-    f.write("Hey I am inside with add")
+#second method use writefile method
+try :
+  f = open('myfile.txt','w')
+  lines = ['line1 \n','line2 \n','line3 \n']
+  f.writelines(lines)  
+  f.close()
 
 except Exception as e :
-    print("error show",e)
+    print("add Error",e)    
+
+
+
+
+
+
+
+
+
+
+
+
+
