6fc8ab18bdcac65392135317ae0c9d55d1c2df55
sharmasuper <mohit6375sharma@gmail.com>
2024-06-07 01:01:27 +0530

    Local and global variable

--- text.py
+++ text.py
@@ -1,13 +1,12 @@
-import  os
+x = 10
+def my_function() :
+    global x 
+    x = 4
+    y = 5 # local variable 
+    print(y)
 
-# if(not os.path.exists('data')):    
-# os.mkdir('data')
-try :
-   for i in range(0, 100):
-      os.rename(f"data/day{i+1}.txt",f"data/mohit{i+1}.txt") 
-
-except Exception as e :
-     print("add",e)
+my_function()
+print(x)
 
 
 
