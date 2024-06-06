32c68e21dc965bb0ce5b3f93c32e7b566d9198dd
sharmasuper <mohit6375sharma@gmail.com>
2024-06-07 01:36:37 +0530

    file Io in python

--- text.py
+++ text.py
@@ -1,15 +1,16 @@
-x = 10
-def my_function() :
-    global x 
-    x = 4
-    y = 5 # local variable 
-    print(y)
+try:
 
-my_function()
-print(x)
+#  f = open('data.txt','r') reading file
+#  text = f.read()
+#  print(text)
 
+#  f = open('myfile.txt','a')
+#  f.write('hello,world  add!')  # new file have create and when we run agin compiler this text agin add myfile.txt
+ 
+#  f.close()
+ # new method
+ with open('mynewfile.txt','a') as f :
+    f.write("Hey I am inside with add")
 
-
-
-
-
+except Exception as e :
+    print("error show",e)
