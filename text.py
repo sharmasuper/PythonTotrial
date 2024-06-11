4de7a741e918a57418ef8413d65e523908b0a650
sharmasuper <mohit6375sharma@gmail.com>
2024-06-11 23:32:07 +0530

    decorator

--- text.py
+++ text.py
@@ -1,30 +1,20 @@
-difference between is operator and ==
-is operator - is operator check exact memory location
-== operator - == operator check only value
-a = 4
-b = "4"
-print( a is b) #fasle exact location on memory
-print(a == b)  #fasle value
+def greet(fx) :
+    
+    def mfx() :
+        print('Good Morning')
+        fx()
+        print('Thanks for using this function')
+    return mfx
 
-a = [1,2,43]
-b = [1,2,43]
-print(a is b) #false
-print(a == b) #true
+@greet
+def hello() :
+    print('hello world') 
+@greet #yya greet(hello)()
+def add() :
+    print("mohit")
 
-a = 3
-b = 3
-print(a is b)
-print(a == b)
-
-a = (1,2)
-b = (1,2)  #beacause touple is immutable
-print(a is b)
-print(a == b)
-
-
-
-
-
+add()
+hello()
 
 
 
