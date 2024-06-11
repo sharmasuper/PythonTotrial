3feb9246c321956cf9e8c1e5f7be589281efa4b6
sharmasuper <mohit6375sharma@gmail.com>
2024-06-12 01:33:49 +0530

    alternative constructor

--- text.py
+++ text.py
@@ -1,19 +1,20 @@
-class Employee:
-    def __init__(self, name, id):
+
+
+class Employees :
+    def __init__(self, name, age):
         self.name = name
-        self.id = id
-    def showDetails(self) :
-        print(f" the name of employee :,{self.id} is {self.name}")
+        self.salary = age
+    @classmethod
 
-class Programmer(Employee) :
-    def showLanguage(self) :
-        print("the default language is python",self.name)
+    def fromstr(cls,string) :
+        return cls(string.split("-")[0],string.split("-")[1])
 
+e1 = Employees('Harry',120000)
+print(e1.name)  
+print(e1.salary) 
+string = "John-12000" 
+e2 = Employees.fromstr(string)     
+print(e2.name)  
+print(e2.salary) 
 
-e1 = Employee('Rohan Das',400)
-
-e1.showDetails()
-e2 = Programmer('mohit',2)
-e2.showDetails()
-e2.showLanguage()
 
