b3eae0f395d180417a12f85e712a689c1cd241ea
sharmasuper <mohit6375sharma@gmail.com>
2024-06-15 22:51:43 +0530

    asyncIo method

--- text.py
+++ text.py
@@ -1,36 +1,30 @@
-# # import re module for regular expression
+import time
+import asyncio
+async def function1() :
+    # time.sleep(3)
+    await asyncio.sleep(1)
+    return "Mohit function 1"
+    print("func 1")
 
-import re
-pattern = r"[A-Z]+his"    # raw string
-text = '''This project has currently been taken over by Tung Luong as a 
-graduate project and would love to get feedback on any of the information as well as would love contributions. So please feel 
-free to edit appropriately as well as let me know how I am doing.
-This is a textbook for teaching the principles and practice of proteomics for 
-undergraduate students in the life sciences. The focus is on the analytical methods and 
-data analysis for protein separation, quantitation and identification. We will begin with older
- methods (chromatography, electrophoresis) and move 
-to more modern approaches (chips, for example) that are being used for high throughput proteomics.'''
+async def function2 () :
+    # time.sleep(3)
+    await asyncio.sleep(1)
+    print("func 2")
+    return "Mohit function 2"
 
-# match = re.search(pattern,text)
-# print(match)
+async def function3() :
+    # time.sleep(3)
+    await asyncio.sleep(4)
+    print("func 3")     
+    return "function 3 function 3" 
 
-# but yai frist accurance hu return kartha h
+async def main() :
+  L =await asyncio.gather(function1(),function2(),function3()) 
+  print(L) 
+#  task = asyncio.create_task(function1())   
+ ##  await function1()
+#  await function2()
+#  await function3()  
 
-
-matches = re.finditer(pattern,text)
-# print(matches)
-
-for match in matches :
-    print(match)
-    print(match.span())
-    # print(match.span()[0])
-    print(text[match.span()[0]:match.span()[1]])
-
-
-
-
-
-
-
-
-
+asyncio.run(main()) 
+  
