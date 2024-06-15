9ba31cd6fbb6169e692e28bc30bfd3bc0c010969
sharmasuper <mohit6375sharma@gmail.com>
2024-06-15 21:32:16 +0530

    request method

--- text.py
+++ text.py
@@ -1,2 +1,16 @@
-import shutil
-shutil.copy('text.py','text2.py')
+import requests
+# response = requests.get("https://www.geeksforgeeks.org/shutil-module-in-python/")
+# print(response.text)
+# this method we get html page
+url = "https://jsonplaceholder.typicode.com/posts"
+data = {
+    "title":"foo",
+    "body":"bar",
+    "userId":1
+}
+headers =  {
+    'Content-type' :'application/json;charset=UTF-8',
+
+}
+response = requests.post(url,headers = headers,json=data)
+print(response.text)
