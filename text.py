f980819e04f42def51b3aa8d682a622d2d5525ac
sharmasuper <mohit6375sharma@gmail.com>
2024-06-15 20:26:19 +0530

    Walrus Operator

--- text.py
+++ text.py
@@ -1,36 +1,17 @@
-# #Time module
-# import time
-# def usingWhile () :
-#     i = 0
-#     while i<5000 :
-#         i = i+1
-#         print(i)
+# Walrus Operator
+a = True
+# print(a=False) invalid
+print(a:= False)  # outout - false
 
-# def usingFor() :
-#     for i in range(5000) :
-#         print(i) 
+numbers = [1,2,3,4,5]
+while(n:= len(numbers))>0 :
+    print(n)
+    numbers.pop()
+foods = list()
+while True :
+    food = input("What food ")    
+    if food == "quit" :
+        break 
+    foods.append(food)
 
-
-# init = time.time() # return the time in second
-# usingFor()
-# t1 = time.time() - init
-# init = time.time()
-# # print(time.time()-init)
-# # end = time.time()
-# usingWhile()
-# print(time.time() - init)
-# print(t1)
-
-# import time
-# print(4)
-# time.sleep(3)
-# print("this is printed after 3 seconds")
-
-
-import time
-t = time.localtime()
-formatted_time = time.strftime("%Y-%m-%d %H:%M:%S",t)
-print(formatted_time)
-
-
-
+print( "print add",foods)    
