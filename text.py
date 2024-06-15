65a5a924ce437da3e936e15878dd51ca98aec7d8
sharmasuper <mohit6375sharma@gmail.com>
2024-06-15 22:12:52 +0530

    chaching function

--- text.py
+++ text.py
@@ -1,25 +1,30 @@
-# Generators in python are special type of function that allow you to create an iterators sequence of value. 
-# A Generators function return a generatos object .
-# which can be used to generate the value one-by-one as you iterate over it.
-# Generators are a powerful tool for working with large or complex data sets, as they allow ypu to generate the value
-# on - the fly , rather than having to create and store the entire sequence in memory
+# function Caching - jab ek function ek hu value ai liy kahi bar call hota ---
+import functools
+import time
+@functools.lru_cache(maxsize=None)
 
-# yeild method-  yeild method create a generator and return a suspended execution 
-# ex- 
-def my_generator():
-    for i in range(5):
-        yield i  
-gen =  my_generator()
-print(next(gen))
-print(next(gen))
-print(next(gen))
-print(next(gen))
+def fx(n) :
+    time.sleep(1)
+    return n*5
 
 
+print(fx(20))
+print("done for 20")    
+
+print(fx(2))
+print("done for 20")    
+
+print(fx(6))
+print("done for 20") 
+print("second call memoize function")   
 
 
+print(fx(20))
+print("done for 20")    
 
+print(fx(2))
+print("done for 20")    
 
-
-
+print(fx(6))
+print("done for 20") 
 
