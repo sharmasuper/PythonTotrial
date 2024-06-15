c60c961dc8c9bd5082c9f3e8325074a1b7f8c9ab
sharmasuper <mohit6375sharma@gmail.com>
2024-06-15 23:30:13 +0530

    multhreading

--- text.py
+++ text.py
@@ -1,30 +1,26 @@
+import threading
 import time
-import asyncio
-async def function1() :
-    # time.sleep(3)
-    await asyncio.sleep(1)
-    return "Mohit function 1"
-    print("func 1")
 
-async def function2 () :
-    # time.sleep(3)
-    await asyncio.sleep(1)
-    print("func 2")
-    return "Mohit function 2"
+def func(seconds) :
+    print(f"sleeping for {seconds} seconds")
+    time.sleep(seconds)
+#Normal code
+time1 = time.perf_counter()
+# func(4)
+# func(2) 
+# func(1)
+time2 = time.perf_counter()
+# print(time2-time1)
+# same code using thread
+t1 = threading.Thread(target=func,args=[4])
+t2 = threading.Thread(target=func,args=[2])
+t3 = threading.Thread(target=func,args=[1])
 
-async def function3() :
-    # time.sleep(3)
-    await asyncio.sleep(4)
-    print("func 3")     
-    return "function 3 function 3" 
+t1.start()
+t2.start()
+t3.start()
+print(time2-time1)
 
-async def main() :
-  L =await asyncio.gather(function1(),function2(),function3()) 
-  print(L) 
-#  task = asyncio.create_task(function1())   
- ##  await function1()
-#  await function2()
-#  await function3()  
-
-asyncio.run(main()) 
-  
+t1.join() # means mai tab tak rukana chahta hu jab tak t1 end na ho jay
+t2.join()
+t3.join()
