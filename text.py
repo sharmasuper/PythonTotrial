b2817bf2f23097c4082d747272d285a166e26e2b
sharmasuper <mohit6375sharma@gmail.com>
2024-06-15 20:39:09 +0530

    shutil module

--- text.py
+++ text.py
@@ -1,17 +1,2 @@
-# Walrus Operator
-a = True
-# print(a=False) invalid
-print(a:= False)  # outout - false
-
-numbers = [1,2,3,4,5]
-while(n:= len(numbers))>0 :
-    print(n)
-    numbers.pop()
-foods = list()
-while True :
-    food = input("What food ")    
-    if food == "quit" :
-        break 
-    foods.append(food)
-
-print( "print add",foods)    
+import shutil
+shutil.copy('text.py','text2.py')
