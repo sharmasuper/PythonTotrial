cbb9eb9fe28210a011494e7df5e8665d32cd8242
sharmasuper <mohit6375sharma@gmail.com>
2024-06-15 22:31:13 +0530

    regular expresstion

--- text.py
+++ text.py
@@ -1,30 +1,36 @@
-# function Caching - jab ek function ek hu value ai liy kahi bar call hota ---
-import functools
-import time
-@functools.lru_cache(maxsize=None)
+# # import re module for regular expression
 
-def fx(n) :
-    time.sleep(1)
-    return n*5
+import re
+pattern = r"[A-Z]+his"    # raw string
+text = '''This project has currently been taken over by Tung Luong as a 
+graduate project and would love to get feedback on any of the information as well as would love contributions. So please feel 
+free to edit appropriately as well as let me know how I am doing.
+This is a textbook for teaching the principles and practice of proteomics for 
+undergraduate students in the life sciences. The focus is on the analytical methods and 
+data analysis for protein separation, quantitation and identification. We will begin with older
+ methods (chromatography, electrophoresis) and move 
+to more modern approaches (chips, for example) that are being used for high throughput proteomics.'''
 
+# match = re.search(pattern,text)
+# print(match)
 
-print(fx(20))
-print("done for 20")    
-
-print(fx(2))
-print("done for 20")    
-
-print(fx(6))
-print("done for 20") 
-print("second call memoize function")   
+# but yai frist accurance hu return kartha h
 
 
-print(fx(20))
-print("done for 20")    
+matches = re.finditer(pattern,text)
+# print(matches)
 
-print(fx(2))
-print("done for 20")    
+for match in matches :
+    print(match)
+    print(match.span())
+    # print(match.span()[0])
+    print(text[match.span()[0]:match.span()[1]])
 
-print(fx(6))
-print("done for 20") 
+
+
+
+
+
+
+
 
