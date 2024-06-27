2b858f546a2c49648547bd5a99c2ff2663d74c0a
sharmasuper <mohit6375sharma@gmail.com>
2024-06-27 10:24:05 +0530

    asyncio.AbstractEventLoopPolicy method for loop

--- text.py
+++ text.py
@@ -1,30 +1,15 @@
 import asyncio 
-async def coroutine1() :
-    print('Coroutine 1 is starting')
-    await asyncio.sleep(1)
-    print('Coroutine 1 is done')
 
-async def coroutine2() :
-    print('Coroutine 2 is startine')
-    await asyncio.sleep(1)
-    print('Coroutine 2 is done')
-
-# create a event loop
+async def Hello():
+  print("Hello start")
+  await asyncio.sleep(1) 
+  print('World')
 
 async def main() :
-    print('Creating tasks')
-    task1 = asyncio.create_task(coroutine1())
-    task2 = asyncio.create_task(coroutine2())
+    loop = asyncio.get_event_loop()
+    print("current evnet loop ",loop)
+    await Hello()
 
-    print('Tasks created , waiting for completion')
-    await asyncio.gather(task1,task2)
-
-# Run the event loop 
-
-if __name__ == '__main__':
-    asyncio.run(main())
-
-
-
-
-
+if __name__ =="__main__" :
+    asyncio.run(main())    
+ 
