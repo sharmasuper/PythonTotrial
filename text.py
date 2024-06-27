4f4906713c2edf512f25ffdfa50c20d3e7f59914
sharmasuper <mohit6375sharma@gmail.com>
2024-06-27 06:31:31 +0530

    asyncIo method in python

--- text.py
+++ text.py
@@ -1,26 +1,12 @@
-import threading
-import time
+import asyncio
+try :
+   async def main() :
+    print("await before")
+    await asyncio.sleep(3)
+    print("hello")
+    
+    print("...world") 
 
-def func(seconds) :
-    print(f"sleeping for {seconds} seconds")
-    time.sleep(seconds)
-#Normal code
-time1 = time.perf_counter()
-# func(4)
-# func(2) 
-# func(1)
-time2 = time.perf_counter()
-# print(time2-time1)
-# same code using thread
-t1 = threading.Thread(target=func,args=[4])
-t2 = threading.Thread(target=func,args=[2])
-t3 = threading.Thread(target=func,args=[1])
-
-t1.start()
-t2.start()
-t3.start()
-print(time2-time1)
-
-t1.join() # means mai tab tak rukana chahta hu jab tak t1 end na ho jay
-t2.join()
-t3.join()
+   asyncio.run(main())      
+except Exception as e:
+  print("show error ",e)
