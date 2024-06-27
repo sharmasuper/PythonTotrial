04edd671dcc8d2de23d085ece38d47568241fa3d
sharmasuper <mohit6375sharma@gmail.com>
2024-06-27 10:51:27 +0530

    set_event_loop method

--- text.py
+++ text.py
@@ -1,15 +1,41 @@
-import asyncio 
+# set_event_loop(loop) method example in python in asyncio
+# The set_event_loop(loop) method in asyncio is used to set a specific event loop instance as the current event loop. This can be useful when you want to manage or switch between different event loops programmatically. Here’s an example that demonstrates the usage of set_event_loop(loop):
 
-async def Hello():
-  print("Hello start")
-  await asyncio.sleep(1) 
-  print('World')
+
+
+import asyncio
+
+async def countdown() :
+    for i in range(5,0,-1) :
+        print(f"countdown : {i}")
+        await asyncio.sleep(1)
+
+loop1 = asyncio.new_event_loop()
+loop2 = asyncio.new_event_loop()
+
+# Run coroutine in loop1
+async def run_coroutine_in_loop1() :
+    asyncio.set_event_loop(loop1)
+    print("Running coroutine in loop1")
+    await countdown()
+
+async def run_coroutine_in_loop2() :
+    asyncio.set_event_loop(loop2)
+    print("Runnig coroutine in loop2")
+    await countdown()
+
 
 async def main() :
-    loop = asyncio.get_event_loop()
-    print("current evnet loop ",loop)
-    await Hello()
+    print("Starting countdown in loop1")
+    await run_coroutine_in_loop1()
 
-if __name__ =="__main__" :
-    asyncio.run(main())    
- 
+    print("\n Starting countdown in loop2 ")
+    await run_coroutine_in_loop2()
+
+
+if __name__ == '__main__':
+    asyncio.run(main()) 
+
+
+
+   
