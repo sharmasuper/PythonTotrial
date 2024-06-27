f5c3cee87ac7eec99bdf50681a13b2b1c97bde4e
sharmasuper <mohit6375sharma@gmail.com>
2024-06-27 11:38:34 +0530

    asyncIo.Task meyhod

--- text.py
+++ text.py
@@ -1,41 +1,40 @@
-# set_event_loop(loop) method example in python in asyncio
-# The set_event_loop(loop) method in asyncio is used to set a specific event loop instance as the current event loop. This can be useful when you want to manage or switch between different event loops programmatically. Here’s an example that demonstrates the usage of set_event_loop(loop):
-
-
+# In Python's asyncio module, the Task class is used to wrap 
+# coroutines in a way that they can be run concurrently as part of an
+#  event loop. This class allows you to schedule coroutines and manage
+#   their execution.
 
 import asyncio
 
-async def countdown() :
-    for i in range(5,0,-1) :
-        print(f"countdown : {i}")
-        await asyncio.sleep(1)
+async def coro1() :
+    for i in range(3) :
+        print(f"coro1 iteration {i}")
+        await asyncio.sleep(1) 
 
-loop1 = asyncio.new_event_loop()
-loop2 = asyncio.new_event_loop()
-
-# Run coroutine in loop1
-async def run_coroutine_in_loop1() :
-    asyncio.set_event_loop(loop1)
-    print("Running coroutine in loop1")
-    await countdown()
-
-async def run_coroutine_in_loop2() :
-    asyncio.set_event_loop(loop2)
-    print("Runnig coroutine in loop2")
-    await countdown()
-
+async def coro2() :
+    for i in range(3) :
+        print(f"coro2 iteration {i}")
+        await asyncio.sleep(1.5)
 
 async def main() :
-    print("Starting countdown in loop1")
-    await run_coroutine_in_loop1()
+    # Get the current event loop 
+    # loop = asyncio.get_event_loop()
+    # Create a tasks using asyncio.Task 
+    # task1 = asyncio.Task(coro1(),loop=loop,name ="task1")
+    # task2 = asyncio.Task(coro2(),loop=loop,name='task2') 
+    # esai bhi kar sktai h
+    task1 = asyncio.create_task(coro1())
+    task2 = asyncio.create_task(coro2())
+    await asyncio.gather(task1,task2)
 
-    print("\n Starting countdown in loop2 ")
-    await run_coroutine_in_loop2()
+if __name__ == "__main__" :
+  asyncio.run(main())    
+
+  
 
 
-if __name__ == '__main__':
-    asyncio.run(main()) 
 
 
 
-   
+
+
+
