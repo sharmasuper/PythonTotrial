c1c8cd40c89e22f76535827e9c3ec580833b5a2e
sharmasuper <mohit6375sharma@gmail.com>
2024-06-30 15:44:41 +0530

    asyncio.Condition mai notyfy meyhod use example

--- text.py
+++ text.py
@@ -14,40 +14,33 @@
 # Producer changed state.value to 1
 # Consumer received notification: state.value=1
 
-import asyncio
+
+import asyncio 
 
 class SharedState :
     def __init__(self) :
-        self.value = 0 
+        self.value = 0
 
 async def consumer(cond,state) :
-    async with cond :         
-        await cond.wait()
-        print(f"Consumer received notified : state.value is {state.value}")
-
-async def Producer(cond,state) :
-    await cond.acquire()  # Explicity acquire the condition's lock
-    try :
-        await asyncio.sleep(1) # Simulate some work 
-        state.value +=1 
-        print(f"producer changed state.value to {state.value}")
-        cond.notify_all()
-    finally :     
-           cond.release()  # Always release the look 
+        async with cond :
+            print("Consumer waiting for condition")
+            await cond.wait()
+            print(f"Consumer recived notificaiton value is state.value = {state.value}")
 
 
+async def producer(cond,state) :
+        await asyncio.sleep(1)
+        async with  cond :
+            state.value += 1
+            print(f"Producer changed state.value to {state.value}")
+            #cond.notify() # Notify one waiting consumer 
+            cond.notify_all() # Uncoment to notify all waiting consumers
 
-async def main () :
-  cond = asyncio.Condition()
-  state = SharedState()
-  consumer_task = asyncio.create_task(consumer(cond,state))
-  producer_task = asyncio.create_task(Producer(cond,state))
-  await asyncio.gather(consumer_task,producer_task)
+async def main() :
+    cond = asyncio.Condition()
+    state = SharedState()
 
-asyncio.run(main())  
-
-
-
-
-
-
+    consumer_task = asyncio.create_task(consumer(cond,state))
+    prodcer_task = asyncio.create_task(producer(cond,state))
+    await asyncio.gather(consumer_task,prodcer_task)
+asyncio.run(main())     
