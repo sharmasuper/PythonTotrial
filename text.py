8ab74938cfd1d1437631fcc93962909d19204f99
sharmasuper <mohit6375sharma@gmail.com>
2024-06-30 19:19:34 +0530

    datagram method successfully created

--- text.py
+++ text.py
@@ -1,17 +1,50 @@
-asyncio.Condition method  
-Certainly! The asyncio.Condition class in Python is used to 
-wait for some condition to be met. It works similarly to threading
-.Condition but is designed for use with asyncio's event loop 
-and coroutines.
+asyncio.DatagramProtocol
+method => 1 datagram_received   2 error_recived
 
-asyncio.condition class kai method
-acquire   , notify
-locked , notify_all, locked, release ,wait , wait for
+import asyncio
+class EchoClientProtocol(asyncio.DatagramProtocol) :
+    def __init__(self,message,loop) :
+        self.message = message
+        self.loop = loop 
+
+    def connection_made(self,tranport) :
+        self.tranport = tranport 
+        print("f senfing message :",self.message)
+        self.tranport.sendto(self.message.encode())
+
+    def datagram_received(self,data,addr) :
+        message = data.encode()
+        # data.encode() per nahi chal raha h yai
+        print(f"Recived {message} from {addr}")
+        print("Closing tranport")
+        self.tranport.close()
+    def connection_lost(self,exc) :
+        print("Conneciton closed")
+        self.loop.stop()
 
 
-output is
-Consumer is waiting for condition.
-Producer changed state.value to 1
-Consumer received notification: state.value=1
+async def main() :
+    loop = asyncio.get_running_loop()
+    message = "Hello,World !"
+    protocol = EchoClientProtocol(message,loop)
+    transport,ALLcallingFunction  = await loop.create_datagram_endpoint(
+        lambda : protocol,
+        remote_addr = ('127.0.0.1',9999)
+    )
+    ALLcallingFunction.connection_made(transport)
+    ALLcallingFunction.datagram_received("Hello,this is a test",('127.0.0.1'))
+    # ALLcallingFunction.connection_lost()
+ #transport,_ this _ method issai hum any function call kar sktai h
 
-successfully asyncio.Condition method
+    try : 
+      await asyncio.sleep(2)
+    finally : 
+            transport.close()
+asyncio.run(main())   #//output is f senfing message : Hello,World !
+#Conneciton closed     
+
+
+              
+
+
+
