f9e199c2187a6edfa816593e39e6665e022883a7
sharmasuper <mohit6375sharma@gmail.com>
2024-07-06 15:32:56 +0530

    future.getloop method

--- text.py
+++ text.py
@@ -1,53 +1,21 @@
-# The asyncio.Future.done() method is used to check if a Future object has been completed, either by setting a result or by being canceled. Here's an example to illustrate its usage
-
-# import asyncio 
-
-# async def main() :
-#     # create a Future object
-#     future = asyncio.Future()
-#     # Schedule a task to complete the future after a delay 
-#     asyncio.create_task(complete_future(future)) 
-#     # Check if the future is done before it is complted 
-#     print(f"Is the future done? {future.done()}")
-#     # await the future to be completed 
-#     await future 
-#     # check it the future is done after it is completd 
-#     print(f"Is the future done? {future.done()}") 
-
-#     #Print the result of the future 
-#     print(f"future result : {future.result()}")
-
-# async def complete_future(future):
-#     await asyncio.sleep(1)
-#     future.set_result("Future is done ")
-
-#      # Run the event loop
-# asyncio.run(main()) 
-# second method 
-
 import asyncio 
 
-async def complete_task(future) :
-      future.set_result("future is done ")
-      asyncio.sleep(1) 
 async def main() :
-    loop = asyncio.get_running_loop()
-    # create a Future object 
-    future = loop.create_future() 
-    t_ask = asyncio.create_task( complete_task(future))
-    await asyncio.gather(t_ask)
-    fu =  future.result()
-    print(fu)
-    print(f"Is the future done? {future.done()}")  
+    # create a future object 
+    future = asyncio.Future()
+    # Get the event loop associated with the Future 
+    loop = future.get_loop()
+    # Print the event loop 
+    print(f"The event loop for this future is : {loop}")
+    # Schedule a task that will complete the future after a delay 
+    asyncio.create_task(complete_future(future)) 
 
+    await future
+    print(f"future result : {future.result()}")
 
-asyncio.run(main())    
-    
+async def complete_future(future) :
+    await asyncio.sleep(2) 
+    future.set_result("Task completed successfully") 
 
+asyncio.run(main())       
 
-
-
-
-
-
-      
