bd0e3811048d4f7c83d4a60192f02b48ca4e085b
sharmasuper <mohit6375sharma@gmail.com>
2024-07-06 14:17:58 +0530

    asyncio.future.add_done_callback method

--- text.py
+++ text.py
@@ -1,21 +1,31 @@
-# The asyncio.Future class in Python is used to represent an eventual
-#  result of an asynchronous operation. It's part of the asyncio 
-#  module, which is used for writing concurrent code using the 
-#  async/await syntax.
+# The add_done_callback method in asyncio.Future allows you to attach
+#  a callback function that will be called when the Future is done 
+#  (either successfully or with an exception).
 
 import asyncio 
 
+def done_callback(fut) :
+    print(f"future is done {fut.result()}")
+
 async def set_future_value(fut) :
-    # await asyncio.sleep(2)
-    fut.set_result('! future is done')
+    await asyncio.sleep(1)
+    fut.set_result("future is done !")
+    print("future value set") 
 
 async def main() :
     loop = asyncio.get_running_loop()
     fut = loop.create_future()
-
-    asyncio.create_task(set_future_value(fut))     
+    fut.add_done_callback(done_callback)
+   
+    asyncio.create_task(set_future_value(fut))
     result = await fut 
-    print(result)
+    print(f" main done with result : {result}")
+
 
 asyncio.run(main())
+
+
+    
+
+
 
