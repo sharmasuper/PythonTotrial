ce89d9d3bc60cfbb655194e6258129f7bb24215c
sharmasuper <mohit6375sharma@gmail.com>
2024-07-06 15:14:41 +0530

    future.done() method

--- text.py
+++ text.py
@@ -1,32 +1,53 @@
-# The asyncio.Future.cancel() method is used to cancel a Future in Python's asyncio library. 
+# The asyncio.Future.done() method is used to check if a Future object has been completed, either by setting a result or by being canceled. Here's an example to illustrate its usage
+
+# import asyncio 
+
+# async def main() :
+#     # create a Future object
+#     future = asyncio.Future()
+#     # Schedule a task to complete the future after a delay 
+#     asyncio.create_task(complete_future(future)) 
+#     # Check if the future is done before it is complted 
+#     print(f"Is the future done? {future.done()}")
+#     # await the future to be completed 
+#     await future 
+#     # check it the future is done after it is completd 
+#     print(f"Is the future done? {future.done()}") 
+
+#     #Print the result of the future 
+#     print(f"future result : {future.result()}")
+
+# async def complete_future(future):
+#     await asyncio.sleep(1)
+#     future.set_result("Future is done ")
+
+#      # Run the event loop
+# asyncio.run(main()) 
+# second method 
 
 import asyncio 
 
+async def complete_task(future) :
+      future.set_result("future is done ")
+      asyncio.sleep(1) 
 async def main() :
-    future = asyncio.Future()
-    # Schedule a task to complete the future after a delay
-    asyncio.create_task(complete_future(future))
-    #cancel the future before it is completed 
-    await asyncio.sleep(1)
-    future.cancel()
-
-    try : 
-        # Await the future to raise the cancellation exception
-        await future 
-    except asyncio.CancelledError :
-         print('Future was cancelled')
+    loop = asyncio.get_running_loop()
+    # create a Future object 
+    future = loop.create_future() 
+    t_ask = asyncio.create_task( complete_task(future))
+    await asyncio.gather(t_ask)
+    fu =  future.result()
+    print(fu)
+    print(f"Is the future done? {future.done()}")  
 
 
-async def complete_future(future):
-    await asyncio.sleep(2)
-    if not future.cancelled():
-        future.set_result("Future is done!")
+asyncio.run(main())    
     
 
-asyncio.run(main())
 
 
 
 
 
 
+      
